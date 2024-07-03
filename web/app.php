96b21a0d62fe145125ac42d0c0e760ececdf4003
david.didnik <david.didnik@leplusduweb.com>
2024-07-03 09:01:30 +0200

    [init] : init old code to analyze it with codacy

---
+++ web/app.php
@@ -0,0 +1,21 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+// The php.ini file already set the timezone, but it's semms to not be inherited by the web server
+date_default_timezone_set('Europe/Paris');
+
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require __DIR__.'/../app/autoload.php';
+include_once __DIR__.'/../var/bootstrap.php.cache';
+
+$kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
+//$kernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+//Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
